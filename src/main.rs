ee1aa98afcdbe7f2e54bfec1c30808e4cb8c8409
kirinokirino <kirinokirino@lunariko.com>
2023-02-01 14:17:12 +0300

    Draw start and finish cells

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use speedy2d::{
     color::Color,
     dimen::UVec2,
-    window::{VirtualKeyCode, WindowCreationOptions, WindowHandler, WindowPosition, WindowSize},
-    Window,
+    window::{
+        KeyScancode, VirtualKeyCode, WindowCreationOptions, WindowHandler, WindowHelper,
+        WindowPosition, WindowSize,
+    },
+    Graphics2D, Window,
 };
 
 mod grid;
@@ -20,38 +23,40 @@ fn main() {
             .with_transparent(true),
     )
     .expect("Wasn't able to create a window!");
-    window.run_loop(App::new(48, 36));
+    let cell_size = 10.0;
+    let cells_width = WINDOW_X as f32 / cell_size;
+    let cells_height = WINDOW_Y as f32 / cell_size;
+    window.run_loop(App::new(
+        cells_width.round() as usize,
+        cells_height.round() as usize,
+        cell_size,
+    ));
 }
 struct App {
     grid: Grid,
 }
 
 impl App {
-    pub fn new(width: usize, height: usize) -> Self {
+    pub fn new(width: usize, height: usize, scale: f32) -> Self {
         Self {
-            grid: Grid::new(width, height),
+            grid: Grid::new(width, height, scale),
         }
     }
 }
 
 impl WindowHandler for App {
-    fn on_draw(
-        &mut self,
-        helper: &mut speedy2d::window::WindowHelper<()>,
-        graphics: &mut speedy2d::Graphics2D,
-    ) {
+    fn on_draw(&mut self, helper: &mut WindowHelper<()>, graphics: &mut Graphics2D) {
         graphics.clear_screen(Color::from_rgb(0.8, 0.9, 1.0));
-        graphics.draw_circle((100.0, 100.0), 75.0, Color::BLUE);
-
+        self.grid.draw(graphics);
         // Request that we draw another frame once this one has finished
         helper.request_redraw();
     }
 
     fn on_key_down(
         &mut self,
-        helper: &mut speedy2d::window::WindowHelper<()>,
-        virtual_key_code: Option<speedy2d::window::VirtualKeyCode>,
-        scancode: speedy2d::window::KeyScancode,
+        helper: &mut WindowHelper<()>,
+        virtual_key_code: Option<VirtualKeyCode>,
+        scancode: KeyScancode,
     ) {
         if let Some(key_code) = virtual_key_code {
             match key_code {
